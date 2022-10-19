e92387c64249600045978c73f735e735e63cd5b1
pangyu <1510397456@qq.com>
2022-10-19 11:42:22 +0800

    Prohibit data modification tracking, to remove the warning

--- config.py
+++ config.py
@@ -6,3 +6,6 @@ USERNAME = 'root'
 PASSWORD = 'lpy..2002'
 DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
 SQLALCHEMY_DATABASE_URI = DB_URI
+
+# Prohibit data modification tracking
+SQLALCHEMY_TRACK_MODIFICATIONS = True
