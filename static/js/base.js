049907e04a40f82c52cf8b87478afc861c83c797
pangyu <1510397456@qq.com>
2022-10-20 20:06:48 +0800

    make the below content adaptive

--- static/js/base.js
+++ static/js/base.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    adaptive();
+
     // change the saying every 5 seconds
     $.get('/static/sayings.txt', function(data) {
         sayings = data.split("\n");
@@ -9,16 +11,53 @@ $(document).ready(function() {
         }, 5000); // in ms.
     });
 
+    // make the side bar expandable and collapsible
     $(".topnav-left>button:first-child").on("click", function() {
         var sidebarLeft = $("#sidebar").css('left');
         if (sidebarLeft == '0px') {
+            $('#sidebar').css('position', 'fixed');
             $("#sidebar").css('left', '-250px');
             $("#mainContent").css('margin-left', '0px');
             $("#mainContent").css('width', '100%');
+            $("#sidebar").css('box-shadow', 'none');
         } else {
-            $("#sidebar").css('left', '0px');
-            $("#mainContent").css('margin-left', '250px');
-            $("#mainContent").css('width', calc(100%-sidebarLeft));
+            if ($(window).width() >= 700) {
+                var mainContentWidth = $(window).width() - 250;
+                $('#sidebar').css('position', 'fixed');
+                $("#sidebar").css('left', '0px');
+                $("#mainContent").css('margin-left', '250px');
+                $("#mainContent").css('width', mainContentWidth);
+            } else {
+                $("#mainContent").css('margin-left', '0px');
+                $("#mainContent").css('width', '100%');
+                $('#sidebar').css('position', 'absolute');
+                $("#sidebar").css('left', '0px');
+                $("#sidebar").css('box-shadow', '4px 5px 5px #888888');
+            }
         }
     });
+
+    // make it suitable for small devices
+    $(window).resize(function(){
+        adaptive();
+    });
+
 });
+
+function adaptive() {
+    var sidebarLeft = $("#sidebar").css('left');
+    if (sidebarLeft == '0px') {
+        if ($(window).width() < 700) {
+            $('#sidebar').css('position', 'fixed');
+            $("#sidebar").css('left', '-250px');
+            $("#mainContent").css('margin-left', '0px');
+            $("#mainContent").css('width', '100%');
+            $("#sidebar").css('box-shadow', 'none');
+        }
+    }
+    if ($(window).width() < 625 ) {
+        $("#saying").css('display','none');
+    }else{
+        $("#saying").css('display','');
+    }
+}
