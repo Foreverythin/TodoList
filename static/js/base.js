81d4540ea7ab06fb6f1108de078679153d3f0b4d
pangyu <1510397456@qq.com>
2022-10-20 17:58:31 +0800

    set the layout of the side bar and the main content

--- static/js/base.js
+++ static/js/base.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
-    // read txt one line by one line
+    // change the saying every 5 seconds
     $.get('/static/sayings.txt', function(data) {
-        alert(data);
+        sayings = data.split("\n");
+        // set interval before reload a new saying
+        var now = 0; // current reading line
+        setInterval(function() {
+            $("#saying").html(sayings[now++%sayings.length]);
+        }, 5000); // in ms.
+    });
+
+    $(".topnav-left>button:first-child").on("click", function() {
+        var sidebarLeft = $("#sidebar").css('left');
+        if (sidebarLeft == '0px') {
+            $("#sidebar").css('left', '-250px');
+            $("#mainContent").css('margin-left', '0px');
+            $("#mainContent").css('width', '100%');
+        } else {
+            $("#sidebar").css('left', '0px');
+            $("#mainContent").css('margin-left', '250px');
+            $("#mainContent").css('width', calc(100%-sidebarLeft));
+        }
     });
 });
