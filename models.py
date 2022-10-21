f1617e83461e11ccd4ad9b38d13444b8ae4f3288
pangyu <1510397456@qq.com>
2022-10-22 00:48:26 +0800

    change id to uid in the table User

--- models.py
+++ models.py
@@ -2,6 +2,6 @@ from exts import db
 
 class User(db.Model):
     __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    uid = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(100), nullable=False)
